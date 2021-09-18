afad8010d2260147c5f4fcbd73b74fb3ab1933d5
avi9a <73037370+avi9a@users.noreply.github.com>
2021-09-18 16:50:01 +0300

    feat: game master

---
+++ JohnLemon/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster instance;
+    public Vector3 lastCheckPointPosition;
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
