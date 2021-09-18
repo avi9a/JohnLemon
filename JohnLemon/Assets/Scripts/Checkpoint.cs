d2bd42f3446ade086475ab70942c52acb0ba6bba
avi9a <73037370+avi9a@users.noreply.github.com>
2021-09-18 16:49:35 +0300

    feat: checkpoint

---
+++ JohnLemon/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    private GameMaster gameMaster;
+    void Start() {
+        gameMaster = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            gameMaster.lastCheckPointPosition = transform.position;
+        }
+    }
+}
